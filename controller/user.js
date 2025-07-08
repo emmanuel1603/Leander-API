c7904a77a4dff7edf5f6c792b08a5fe9a9fa4467
Emmanuel Mariño <emmanuel.m@gmail.com>
2025-07-08 18:20:26 -0400

    userfix

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var user = require('../models/user');
+var User = require('../models/user');
 var jwt = require('jwt-simple');
 var moment = require('moment');
 var fs = require('fs');
